037ec83044c6b098c823cf646c991bd63586cccc
huhao <915664508@qq.com>
2020-06-11 11:23:12 +0800

    chore: 修复格式错误

--- .vuepress/nav/index.js
+++ .vuepress/nav/index.js
@@ -1,46 +1,46 @@
 module.exports = [
-    {
-        text: "链优品",
-        items: [
-            {text: "移动端", link: "/api/lianyoupin/"},
-            {text: "后台管理", link: "/admin/lianyoupin/"},
-            {text: "商户管理", link: "/merchant/"}
-        ]
-    },
-    {
-        text: "iMeet",
-        items: [
-            {text: "移动端", link: "/api/imeet/"},
-            {text: "后台管理", link: "/admin/imeet/"}
-        ]
-    },
-    {
-        text: "链聊",
-        items: [
-            {text: "移动端", link: "/api/achat/"},
-            {text: "后台管理", link: "/admin/achat/"}
-        ]
-    },
-    {
-        text: "存链",
-        items: [
-            {text: "移动端", link: "/api/schain/"},
-            {text: "后台管理", link: "/admin/schain/"}
-        ]
-    },
-    {
-        text: "科创商城",
-        items: [
-            {text: "移动端", link: "/api/kcstore/"},
-            {text: "后台管理", link: "/admin/kcstore/"},
-            {text: "商户管理", link: "/merchant/kcstore/"}
-        ]
-    },
-    {
-        text: "火推",
-        items: [
-            {text: "移动端", link: "/api/ht/"},
-            {text: "后台管理", link: "/admin/ht/"},
-        ]
-    }
+  {
+    text: "链优品",
+    items: [
+      { text: "移动端", link: "/api/lianyoupin/" },
+      { text: "后台管理", link: "/admin/lianyoupin/" },
+      { text: "商户管理", link: "/merchant/" }
+    ]
+  },
+  {
+    text: "iMeet",
+    items: [
+      { text: "移动端", link: "/api/imeet/" },
+      { text: "后台管理", link: "/admin/imeet/" }
+    ]
+  },
+  {
+    text: "链聊",
+    items: [
+      { text: "移动端", link: "/api/achat/" },
+      { text: "后台管理", link: "/admin/achat/" }
+    ]
+  },
+  {
+    text: "存链",
+    items: [
+      { text: "移动端", link: "/api/schain/" },
+      { text: "后台管理", link: "/admin/schain/" }
+    ]
+  },
+  {
+    text: "科创商城",
+    items: [
+      { text: "移动端", link: "/api/kcstore/" },
+      { text: "后台管理", link: "/admin/kcstore/" },
+      { text: "商户管理", link: "/merchant/kcstore/" }
+    ]
+  },
+  {
+    text: "火推",
+    items: [
+      { text: "移动端", link: "/api/ht/" },
+      { text: "后台管理", link: "/admin/ht/" }
+    ]
+  }
 ];
